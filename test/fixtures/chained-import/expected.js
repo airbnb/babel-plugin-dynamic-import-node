bb04dd3003b3d22aba644717916fe92b71e528ba
vikr01 <vik120@icloud.com>
2018-09-08 19:01:30 -0700

    fixed all expected.js tests

--- test/fixtures/chained-import/expected.js
+++ test/fixtures/chained-import/expected.js
@@ -1,5 +1,4 @@
 function _interopRequireWildcard(obj) {}
 
 Promise.resolve().then(() => _interopRequireWildcard(require('test-module'))).then(() => Promise.resolve().then(() => _interopRequireWildcard(require('test-module-2'))));
-
 Promise.all([Promise.resolve().then(() => _interopRequireWildcard(require('test-1'))), Promise.resolve().then(() => _interopRequireWildcard(require('test-2'))), Promise.resolve().then(() => _interopRequireWildcard(require('test-3')))]).then(() => {});
